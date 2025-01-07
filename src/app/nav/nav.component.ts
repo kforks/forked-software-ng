f49a2f45008b507e6f1a59b41896f250721f6de4
kforks <kaitlynforks@gmail.com>
2025-01-07 17:00:18 -0600

    nav improvements

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,16 @@
 import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconButton} from '@angular/material/button';
-import {faCodeFork, faMicrochip} from '@fortawesome/free-solid-svg-icons';
-import {FaIconComponent, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {MatTooltip} from '@angular/material/tooltip';
-import {NgOptimizedImage} from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { FaIconComponent, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { MatTooltip } from '@angular/material/tooltip';
+import {NgIf, NgOptimizedImage, NgSwitch, NgSwitchCase} from '@angular/common';
 
 @Component({
-    selector: 'app-nav',
+  selector: 'app-nav',
+  standalone: true,
   imports: [
     MatIconModule,
     MatToolbarModule,
@@ -17,16 +18,23 @@ import {NgOptimizedImage} from '@angular/common';
     FaIconComponent,
     FontAwesomeModule,
     MatTooltip,
-    NgOptimizedImage
+    NgSwitch,
+    NgSwitchCase,
+    NgIf,
+    NgOptimizedImage,
   ],
-    templateUrl: './nav.component.html',
-    styleUrl: './nav.component.scss'
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
   protected readonly faLinkedin = faLinkedin;
+  protected readonly faShare = faShare;
+  activeLink: string | null = null; // Keeps track of the hovered link
+
+  constructor() {}
 
   copyUrlToClipboard(): void {
-    const url = window.location.href; // Get the current page URL
+    const url = window.location.href;
     navigator.clipboard.writeText(url).then(
       () => {
         alert('URL copied to clipboard!');
@@ -37,11 +45,11 @@ export class NavComponent {
     );
   }
 
-  // Function to open LinkedIn profile
   openLinkedIn(): void {
-    window.open('https://www.linkedin.com/in/kaitlyn-forks/', '_blank'); // Opens in a new tab
+    window.open('https://www.linkedin.com/in/kaitlyn-forks/', '_blank');
   }
 
-  protected readonly faCodeFork = faCodeFork;
-  protected readonly faMicrochip = faMicrochip;
+  setActiveLink(link: string | null): void {
+    this.activeLink = link;
+  }
 }
