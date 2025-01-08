a3483fc962f2e1f7b9e9abb71f65326ede87f2b2
kforks <kaitlynforks@gmail.com>
2025-01-08 15:33:51 -0600

    custom font

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,8 @@
 import {Component} from '@angular/core';
-import {NavComponent} from '../nav/nav.component';
 
 @Component({
     selector: 'app-home',
-    imports: [
-        NavComponent
-    ],
+  imports: [],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
