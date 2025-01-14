20caf338b3be54d2ecde17cdd1fe57e5c3b9f29d
kforks <kaitlynforks@gmail.com>
2025-01-13 18:55:20 -0600

    rm unused code in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,26 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { Subscription } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import {NavComponent} from './nav/nav.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
-    selector: 'app-root',
+  selector: 'app-root',
   imports: [HomeComponent, NavComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = 'kaitlyn-forks';
-  currentTheme!: string;
   isSmallScreen: boolean = false;
 
-  private themeSubscription!: Subscription;
-  constructor(
-    // public themeService: ThemeService,
-    private readonly breakpointObserver: BreakpointObserver
-  ) {
+  constructor(private readonly breakpointObserver: BreakpointObserver) {
     this.breakpointObserver
       .observe([Breakpoints.Small, Breakpoints.XSmall])
-      .subscribe((resp: { breakpoints: { [x: string]: unknown; }; }) => {
-        this.isSmallScreen = Object.keys(resp.breakpoints).some(a => resp.breakpoints[a]);
+      .subscribe((resp: { breakpoints: { [x: string]: unknown } }) => {
+        this.isSmallScreen = Object.keys(resp.breakpoints).some(
+          (a) => resp.breakpoints[a],
+        );
       });
-
-    // this.themeSubscription = this.themeService
-    //   .getCurrentTheme()
-    //   .subscribe((theme: string) => {
-    //     this.currentTheme = theme;
-    //   });
-  }
-
-  ngOnDestroy(): void {
-    this.themeSubscription.unsubscribe();
   }
 }
-
-
