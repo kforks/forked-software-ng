97f20ac8d6486a5fad307de72caf146cc4bfba97
kforks <kaitlynforks@gmail.com>
2025-01-26 14:51:18 -0600

    strengths progress

--- src/app/components/strengths-carousel/strengths-carousel.component.ts
+++ src/app/components/strengths-carousel/strengths-carousel.component.ts
@@ -1,62 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
 import { NgForOf } from '@angular/common';
 import { MatCard } from '@angular/material/card';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import {
+  faChess,
+  faHandshake,
+  faLaughBeam,
+  faPeopleRoof,
+  faPersonChalkboard,
+  faTrophy,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-strengths-carousel',
   templateUrl: './strengths-carousel.component.html',
   styleUrls: ['./strengths-carousel.component.scss'],
-  imports: [MatIcon, NgForOf, MatCard],
+  imports: [NgForOf, MatCard, FaIconComponent, MatIconButton, MatTooltip],
 })
 export class StrengthsCarouselComponent implements OnInit {
-  strengths = [
+  visibleStrengths: any[] = [];
+  currentTranslateX = 0;
+  protected readonly faTrophy = faTrophy;
+  strengths: { title: string; icon: IconDefinition; description: string }[] = [
     {
-      title: 'Strategic',
-      icon: 'map',
+      title: 'Strategic<br>(#1 Top Strength)',
+      icon: faChess,
       description:
         'A natural problem-solver, Kaitlyn excels at identifying patterns in complexity and charting the best path forward.',
     },
     {
-      title: 'Woo',
-      icon: 'group',
+      title: 'Winning others over<br>(#2)',
+      icon: faHandshake,
       description:
-        'Kaitlyn thrives in social environments, effortlessly connecting with new people and fostering meaningful conversations.',
+        'Also known as "WOO," this means Kaitlyn thrives in social environments, effortlessly connecting with new people and fostering meaningful conversations.',
     },
     {
-      title: 'Positivity',
-      icon: 'emoji_emotions',
+      title: 'Positivity<br>(#3)',
+      icon: faLaughBeam,
       description:
         'With an optimistic outlook and a contagious energy, Kaitlyn uplifts those around her.',
     },
     {
-      title: 'Developer',
-      icon: 'trending_up',
+      title: 'Developer<br>(#4)',
+      icon: faPersonChalkboard,
       description:
         'Kaitlyn is deeply invested in helping others grow and reach their full potential.',
     },
     {
-      title: 'Empathy',
-      icon: 'favorite',
+      title: 'Empathy<br>(#5)',
+      icon: faPeopleRoof,
       description:
         'Kaitlyn intuitively senses the emotions of those around her, offering genuine understanding and support.',
     },
   ];
 
-  visibleStrengths: any[] = [];
-  currentTranslateX = 0;
+  constructor() {}
 
   ngOnInit() {
     // Initialize visibleStrengths with extra strengths for smooth scrolling
     this.visibleStrengths = [
       ...this.strengths,
-      ...this.strengths.slice(0, 6), // Preload enough items for at least two additional slides
+      ...this.strengths.slice(0, 6), // Preload enough items for at least two additional sets
     ];
   }
 
   prevSlide() {
     if (this.currentTranslateX === 0) {
-      // Preload at least two additional sets at the beginning if needed
+      // Preload at least two additional sets at the beginning for back scroll
       const additionalSet = [...this.strengths.slice(-6)];
       this.visibleStrengths = [...additionalSet, ...this.visibleStrengths];
       this.currentTranslateX += 200; // Adjust to maintain the current slide position
@@ -76,7 +89,6 @@ export class StrengthsCarouselComponent implements OnInit {
         ...additionalSet.slice(0, 6),
       ];
     }
-
     this.currentTranslateX += 100;
   }
 
@@ -89,5 +101,10 @@ export class StrengthsCarouselComponent implements OnInit {
     // Calculate how many slots are remaining before the visibleStrengths array runs out
     const totalVisibleSlots = Math.floor(this.currentTranslateX / 100) * 3 + 3;
     return this.visibleStrengths.length - totalVisibleSlots;
+  }
+
+  openPdf(filePath: string): void {
+    const fullPath = `assets/files/${filePath}`;
+    window.open(fullPath, '_blank');
   }
 }
